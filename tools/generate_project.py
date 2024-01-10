0be006e15475d26c81505ff5bf9351091cd5e508
puddly <32534428+puddly@users.noreply.github.com>
2024-01-10 11:57:42 -0500

    Migrate to `argparse`

--- tools/generate_project.py
+++ tools/generate_project.py
@@ -10,14 +10,10 @@ import copy
 import shutil
 import logging
 import pathlib
+import argparse
 import subprocess
 
 from ruamel.yaml import YAML
-
-SDK = pathlib.Path("~/SimplicityStudio/SDKs/gecko_sdk/").expanduser()
-TOOLCHAIN = "/Applications/Simplicity Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/12.2.rel1_2023.7"
-POSTBUILD = pathlib.Path(__file__).parent / "create_gbl.py"
-
 
 # Matches all known chip and board names. Some varied examples:
 #   MGM210PB32JIA EFM32GG890F512 MGM12P22F1024GA EZR32WG330F128R69 EFR32FG14P231F256GM32
@@ -39,194 +35,293 @@ LOGGER = logging.getLogger(__name__)
 
 yaml = YAML(typ="safe")
 
-manifest_path = pathlib.Path(sys.argv[1])
-output_path = pathlib.Path(sys.argv[2])
-projects_root = manifest_path.parent.parent
 
-manifest = yaml.load(manifest_path.read_text())
+def ensure_folder(path: str | pathlib.Path) -> pathlib.Path:
+    """Ensure that the path exists and is a folder."""
+    path = pathlib.Path(path)
 
-# First, load the base project
-base_project_path = projects_root / manifest["base_project"]
-assert base_project_path.is_relative_to(projects_root)
-(base_project_slcp,) = base_project_path.glob("*.slcp")
-base_project = yaml.load(base_project_slcp.read_text())
+    if not path.is_dir():
+        raise argparse.ArgumentTypeError(f"Folder {path} does not exist")
 
-output_project = copy.deepcopy(base_project)
+    return path
 
-# Strip chip- and board-specific components to modify the base device type
-output_project["component"] = [
-    c for c in output_project["component"] if not CHIP_SPECIFIC_REGEX.match(c["id"])
-]
-output_project["component"].append({"id": manifest["device"]})
 
-# Add new components
-output_project["component"].extend(manifest.get("add_components", []))
-
-# Remove components
-for component in manifest.get("remove_components", []):
-    try:
-        output_project["component"].remove(component)
-    except ValueError:
-        LOGGER.warning(
-            "Component %s is not present in manifest, cannot remove", component
+def get_toolchain_default_path() -> pathlib.Path | None:
+    """Return the path to the toolchain."""
+    if sys.platform == "darwin":
+        return pathlib.Path(
+            "/Applications/Simplicity Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/12.2.rel1_2023.7"
         )
 
-# Extend configuration and C defines
-for input_config, output_config in [
-    (manifest.get("configuration", {}), output_project["configuration"]),
-    (manifest.get("slcp_defines", {}), output_project.get("define", [])),
-]:
-    for name, value in input_config.items():
-        # Values are always strings
-        value = str(value)
+    return None
 
-        # First try to replace any existing config entries
-        for config in output_config:
-            if config["name"] == name:
-                config["value"] = value
-                break
-        else:
-            # Otherwise, append it
-            output_config.append({"name": name, "value": value})
 
-# Copy the base project into the output directory
-output_project_root = output_path / manifest["base_project"]
-output_path.mkdir(exist_ok=True)
-output_project_root.mkdir(exist_ok=True)
+def get_sdk_default_path() -> pathlib.Path | None:
+    """Return the path to the SDK."""
+    if sys.platform == "darwin":
+        return pathlib.Path("~/SimplicityStudio/SDKs/gecko_sdk").expanduser()
 
-shutil.copytree(
-    base_project_path,
-    output_project_root,
-    dirs_exist_ok=True,
-    ignore=lambda dir, contents: [
-        # XXX: pruning `autogen` is extremely important!
-        "autogen",
-        ".git",
-        ".settings",
-        ".projectlinkstore",
-        ".project",
-        ".pdm",
-        ".cproject",
-    ],
-)
+    return None
 
-# Delete the original project file
-(output_project_root / base_project_slcp.name).unlink()
 
-# Delete radio-affecting config: it will be regenerated
-for filename in ["config/sl_rail_util_pa_config.h"]:
-    (output_project_root / filename).unlink()
-
-# Write the new project SLCP file
-with (output_project_root / f"{manifest['base_project']}.slcp").open("w") as f:
-    yaml.dump(output_project, f)
-
-# Create a GBL metadata file
-with pathlib.Path(output_project_root / "gbl_metadata.yaml").open("w") as f:
-    yaml.dump(manifest["gbl"], f)
-
-# Generate a build directory
-build_dir = output_project_root
-cmake_build_root = build_dir / f"{manifest['base_project']}_cmake"
-shutil.rmtree(cmake_build_root, ignore_errors=True)
-
-subprocess.run(
-    [
-        "slc-cli",
-        "generate",
-        "--project-file",
-        (output_project_root / f"{manifest['base_project']}.slcp").resolve(),
-        "--export-destination",
-        build_dir.resolve(),
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--manifest",
+        type=pathlib.Path,
+        required=True,
+        help="Firmware build manifest",
+    )
+    parser.add_argument(
+        "--output-gbl",
+        type=pathlib.Path,
+        required=True,
+        help="Output GBL file",
+    )
+    parser.add_argument(
+        "--build-dir",
+        type=pathlib.Path,
+        required=True,
+        help="Temporary build directory",
+    )
+    parser.add_argument(
         "--sdk",
-        SDK.resolve(),
+        type=ensure_folder,
+        default=get_sdk_default_path(),
+        required=get_sdk_default_path() is None,
+        help="Path to Gecko SDK",
+    )
+    parser.add_argument(
         "--toolchain",
-        "toolchain_gcc",
-        "--output-type",
-        "cmake",
-    ],
-    check=True,
-)
+        type=ensure_folder,
+        default=get_toolchain_default_path(),
+        required=get_toolchain_default_path() is None,
+        help="Path to GCC toolchain",
+    )
+    parser.add_argument(
+        "--postbuild",
+        default=pathlib.Path(__file__).parent / "create_gbl.py",
+        required=False,
+        help="Postbuild executable",
+    )
 
-# Actually search for C defines within config
-unused_defines = set(manifest.get("c_defines", {}).keys())
+    args = parser.parse_args()
 
-for config_root in [output_project_root / "autogen", output_project_root / "config"]:
-    for config_f in config_root.glob("*.h"):
-        config_h_lines = config_f.read_text().split("\n")
-        written_config = {}
-        new_config_h_lines = []
+    manifest = yaml.load(args.manifest.read_text())
 
-        for index, line in enumerate(config_h_lines):
-            for define, value in manifest.get("c_defines", {}).items():
-                if f"#define {define} " not in line:
-                    continue
+    # First, load the base project
+    projects_root = args.manifest.parent.parent
+    base_project_path = projects_root / manifest["base_project"]
+    assert base_project_path.is_relative_to(projects_root)
+    (base_project_slcp,) = base_project_path.glob("*.slcp")
+    base_project = yaml.load(base_project_slcp.read_text())
 
-                define_with_whitespace = line.split(f"#define {define}", 1)[1]
-                alignment = define_with_whitespace[
-                    : define_with_whitespace.index(define_with_whitespace.strip())
-                ]
+    output_project = copy.deepcopy(base_project)
 
-                prev_line = config_h_lines[index - 1]
-                if '#ifndef' in prev_line:
-                    assert re.match(r'#ifndef\s+([A-Z0-9_]+)', prev_line).group(1) == define
+    # Strip chip- and board-specific components to modify the base device type
+    output_project["component"] = [
+        c for c in output_project["component"] if not CHIP_SPECIFIC_REGEX.match(c["id"])
+    ]
+    output_project["component"].append({"id": manifest["device"]})
 
-                    # Make sure that we do not have conflicting defines provided over the command line
-                    assert not any(c["name"] == define for c in output_project["define"])
-                    new_config_h_lines[index - 1] = '#if 1'
-                elif '#warning' in prev_line:
-                    assert re.match(r'#warning ".*? not configured"', prev_line)
-                    new_config_h_lines.pop(index - 1)
+    # Add new components
+    output_project["component"].extend(manifest.get("add_components", []))
 
-                new_config_h_lines.append(f"#define {define}{alignment}{value}")
-                written_config[define] = value
-                unused_defines.remove(define)
-                break
+    # Remove components
+    for component in manifest.get("remove_components", []):
+        try:
+            output_project["component"].remove(component)
+        except ValueError:
+            LOGGER.warning(
+                "Component %s is not present in manifest, cannot remove", component
+            )
+
+    # Extend configuration and C defines
+    for input_config, output_config in [
+        (manifest.get("configuration", {}), output_project["configuration"]),
+        (manifest.get("slcp_defines", {}), output_project.get("define", [])),
+    ]:
+        for name, value in input_config.items():
+            # Values are always strings
+            value = str(value)
+
+            # First try to replace any existing config entries
+            for config in output_config:
+                if config["name"] == name:
+                    config["value"] = value
+                    break
             else:
-                new_config_h_lines.append(line)
+                # Otherwise, append it
+                output_config.append({"name": name, "value": value})
 
-        if written_config:
-            print(f"Patching {config_f} with {written_config}")
-            config_f.write_text("\n".join(new_config_h_lines))
+    # Copy the base project into the output directory
+    args.build_dir.mkdir(exist_ok=True)
 
-if unused_defines:
-    print(f"Defines were unused, aborting: {unused_defines}")
-    sys.exit(1)
+    shutil.copytree(
+        base_project_path,
+        args.build_dir,
+        dirs_exist_ok=True,
+        ignore=lambda dir, contents: [
+            # XXX: pruning `autogen` is extremely important!
+            "autogen",
+            ".git",
+            ".settings",
+            ".projectlinkstore",
+            ".project",
+            ".pdm",
+            ".cproject",
+        ],
+    )
 
-cmake_build_root = build_dir / f"{manifest['base_project']}_cmake"
-cmake_config = cmake_build_root / f"{manifest['base_project']}.cmake"
-fixed_cmake = []
+    # Delete the original project file
+    (args.build_dir / base_project_slcp.name).unlink()
 
-# Fix CMake config quoting bug
-for line in cmake_config.read_text().split("\n"):
-    if ">:SHELL:" not in line and (":-imacros " in line or ":-x " in line):
-        line = '    "' + line.replace(">:", ">:SHELL:").strip() + '"'
+    # Delete radio-affecting config: it will be regenerated
+    for filename in ["config/sl_rail_util_pa_config.h"]:
+        (args.build_dir / filename).unlink()
 
-    fixed_cmake.append(line)
+    # Write the new project SLCP file
+    with (args.build_dir / f"{manifest['base_project']}.slcp").open("w") as f:
+        yaml.dump(output_project, f)
 
-cmake_config.write_text("\n".join(fixed_cmake))
+    # Create a GBL metadata file
+    with pathlib.Path(args.build_dir / "gbl_metadata.yaml").open("w") as f:
+        yaml.dump(manifest["gbl"], f)
 
-# Generate the build system
-subprocess.run(
-    [
-        "cmake",
-        "-G",
-        "Ninja",
-        "-DCMAKE_TOOLCHAIN_FILE=toolchain.cmake",
-        ".",
-    ],
-    cwd=cmake_build_root,
-    env={**os.environ, "ARM_GCC_DIR": TOOLCHAIN, "POST_BUILD_EXE": POSTBUILD},
-    check=True,
-)
+    # Generate a build directory
+    args.build_dir = args.build_dir
+    cmake_build_root = args.build_dir / f"{manifest['base_project']}_cmake"
+    shutil.rmtree(cmake_build_root, ignore_errors=True)
 
-# Build it!
-subprocess.run(
-    [
-        "ninja",
-        # "make",
-        "-C",
-        cmake_build_root,
-    ],
-    check=True,
-)
+    # On macOS `slc` doesn't execute properly
+    slc = shutil.which("slc-cli") or shutil.which("slc")
+
+    if not slc:
+        print("`slc` and/or `slc-cli` not found in PATH")
+        sys.exit(1)
+
+    subprocess.run(
+        [
+            slc,
+            "generate",
+            "--project-file",
+            (args.build_dir / f"{manifest['base_project']}.slcp").resolve(),
+            "--export-destination",
+            args.build_dir.resolve(),
+            "--sdk",
+            args.sdk.resolve(),
+            "--toolchain",
+            "toolchain_gcc",
+            "--output-type",
+            "cmake",
+        ],
+        check=True,
+    )
+
+    # Actually search for C defines within config
+    unused_defines = set(manifest.get("c_defines", {}).keys())
+
+    for config_root in [args.build_dir / "autogen", args.build_dir / "config"]:
+        for config_f in config_root.glob("*.h"):
+            config_h_lines = config_f.read_text().split("\n")
+            written_config = {}
+            new_config_h_lines = []
+
+            for index, line in enumerate(config_h_lines):
+                for define, value in manifest.get("c_defines", {}).items():
+                    if f"#define {define} " not in line:
+                        continue
+
+                    define_with_whitespace = line.split(f"#define {define}", 1)[1]
+                    alignment = define_with_whitespace[
+                        : define_with_whitespace.index(define_with_whitespace.strip())
+                    ]
+
+                    prev_line = config_h_lines[index - 1]
+                    if "#ifndef" in prev_line:
+                        assert (
+                            re.match(r"#ifndef\s+([A-Z0-9_]+)", prev_line).group(1)
+                            == define
+                        )
+
+                        # Make sure that we do not have conflicting defines provided over the command line
+                        assert not any(
+                            c["name"] == define for c in output_project["define"]
+                        )
+                        new_config_h_lines[index - 1] = "#if 1"
+                    elif "#warning" in prev_line:
+                        assert re.match(r'#warning ".*? not configured"', prev_line)
+                        new_config_h_lines.pop(index - 1)
+
+                    new_config_h_lines.append(f"#define {define}{alignment}{value}")
+                    written_config[define] = value
+
+                    if define not in unused_defines:
+                        print(f"Define {define!r} used twice!")
+                        sys.exit(1)
+
+                    unused_defines.remove(define)
+                    break
+                else:
+                    new_config_h_lines.append(line)
+
+            if written_config:
+                print(f"Patching {config_f} with {written_config}")
+                config_f.write_text("\n".join(new_config_h_lines))
+
+    if unused_defines:
+        print(f"Defines were unused, aborting: {unused_defines}")
+        sys.exit(1)
+
+    cmake_build_root = args.build_dir / f"{manifest['base_project']}_cmake"
+    cmake_config = cmake_build_root / f"{manifest['base_project']}.cmake"
+    fixed_cmake = []
+
+    # Fix CMake config quoting bug
+    for line in cmake_config.read_text().split("\n"):
+        if ">:SHELL:" not in line and (":-imacros " in line or ":-x " in line):
+            line = '    "' + line.replace(">:", ">:SHELL:").strip() + '"'
+
+        fixed_cmake.append(line)
+
+    cmake_config.write_text("\n".join(fixed_cmake))
+
+    # Generate the build system
+    subprocess.run(
+        [
+            "cmake",
+            "-G",
+            "Ninja",
+            "-DCMAKE_TOOLCHAIN_FILE=toolchain.cmake",
+            ".",
+        ],
+        cwd=cmake_build_root,
+        env={
+            **os.environ,
+            "ARM_GCC_DIR": args.toolchain,
+            "POST_BUILD_EXE": args.postbuild,
+        },
+        check=True,
+    )
+
+    # Build it!
+    subprocess.run(
+        [
+            "ninja",
+            "-C",
+            cmake_build_root,
+        ],
+        check=True,
+    )
+
+    # Copy the final GBL
+    shutil.copy(
+        src=(cmake_build_root / manifest["base_project"]).with_suffix(".gbl"),
+        dst=args.output_gbl,
+    )
+
+
+if __name__ == "__main__":
+    main()
