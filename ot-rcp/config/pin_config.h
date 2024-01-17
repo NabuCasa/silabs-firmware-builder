e67d4965f3d5baf64bfc3c4e2566083230164eef
puddly <32534428+puddly@users.noreply.github.com>
2024-01-17 13:21:09 -0500

    Add an `ot-rcp` project

---
+++ ot-rcp/config/pin_config.h
@@ -0,0 +1,173 @@
+#ifndef PIN_CONFIG_H
+#define PIN_CONFIG_H
+
+// $[CMU]
+// [CMU]$
+
+// $[LFXO]
+// [LFXO]$
+
+// $[PRS.ASYNCH0]
+// [PRS.ASYNCH0]$
+
+// $[PRS.ASYNCH1]
+// [PRS.ASYNCH1]$
+
+// $[PRS.ASYNCH2]
+// [PRS.ASYNCH2]$
+
+// $[PRS.ASYNCH3]
+// [PRS.ASYNCH3]$
+
+// $[PRS.ASYNCH4]
+// [PRS.ASYNCH4]$
+
+// $[PRS.ASYNCH5]
+// [PRS.ASYNCH5]$
+
+// $[PRS.ASYNCH6]
+// [PRS.ASYNCH6]$
+
+// $[PRS.ASYNCH7]
+// [PRS.ASYNCH7]$
+
+// $[PRS.ASYNCH8]
+// [PRS.ASYNCH8]$
+
+// $[PRS.ASYNCH9]
+// [PRS.ASYNCH9]$
+
+// $[PRS.ASYNCH10]
+// [PRS.ASYNCH10]$
+
+// $[PRS.ASYNCH11]
+// [PRS.ASYNCH11]$
+
+// $[PRS.SYNCH0]
+// [PRS.SYNCH0]$
+
+// $[PRS.SYNCH1]
+// [PRS.SYNCH1]$
+
+// $[PRS.SYNCH2]
+// [PRS.SYNCH2]$
+
+// $[PRS.SYNCH3]
+// [PRS.SYNCH3]$
+
+// $[GPIO]
+// GPIO SWV on PA03
+#ifndef GPIO_SWV_PORT                           
+#define GPIO_SWV_PORT                            gpioPortA
+#endif
+#ifndef GPIO_SWV_PIN                            
+#define GPIO_SWV_PIN                             3
+#endif
+
+// [GPIO]$
+
+// $[TIMER0]
+// [TIMER0]$
+
+// $[TIMER1]
+// [TIMER1]$
+
+// $[TIMER2]
+// [TIMER2]$
+
+// $[TIMER3]
+// [TIMER3]$
+
+// $[USART0]
+// USART0 CTS on PC03
+#ifndef USART0_CTS_PORT                         
+#define USART0_CTS_PORT                          gpioPortC
+#endif
+#ifndef USART0_CTS_PIN                          
+#define USART0_CTS_PIN                           3
+#endif
+
+// USART0 RTS on PC02
+#ifndef USART0_RTS_PORT                         
+#define USART0_RTS_PORT                          gpioPortC
+#endif
+#ifndef USART0_RTS_PIN                          
+#define USART0_RTS_PIN                           2
+#endif
+
+// USART0 RX on PA06
+#ifndef USART0_RX_PORT                          
+#define USART0_RX_PORT                           gpioPortA
+#endif
+#ifndef USART0_RX_PIN                           
+#define USART0_RX_PIN                            6
+#endif
+
+// USART0 TX on PA05
+#ifndef USART0_TX_PORT                          
+#define USART0_TX_PORT                           gpioPortA
+#endif
+#ifndef USART0_TX_PIN                           
+#define USART0_TX_PIN                            5
+#endif
+
+// [USART0]$
+
+// $[USART1]
+// [USART1]$
+
+// $[USART2]
+// [USART2]$
+
+// $[I2C1]
+// [I2C1]$
+
+// $[LETIMER0]
+// [LETIMER0]$
+
+// $[IADC0]
+// [IADC0]$
+
+// $[ACMP0]
+// [ACMP0]$
+
+// $[ACMP1]
+// [ACMP1]$
+
+// $[I2C0]
+// [I2C0]$
+
+// $[PTI]
+// PTI DFRAME on PC05
+#ifndef PTI_DFRAME_PORT                         
+#define PTI_DFRAME_PORT                          gpioPortC
+#endif
+#ifndef PTI_DFRAME_PIN                          
+#define PTI_DFRAME_PIN                           5
+#endif
+
+// PTI DOUT on PC04
+#ifndef PTI_DOUT_PORT                           
+#define PTI_DOUT_PORT                            gpioPortC
+#endif
+#ifndef PTI_DOUT_PIN                            
+#define PTI_DOUT_PIN                             4
+#endif
+
+// [PTI]$
+
+// $[MODEM]
+// [MODEM]$
+
+// $[CUSTOM_PIN_NAME]
+#ifndef _PORT                                   
+#define _PORT                                    gpioPortA
+#endif
+#ifndef _PIN                                    
+#define _PIN                                     0
+#endif
+
+// [CUSTOM_PIN_NAME]$
+
+#endif // PIN_CONFIG_H
+
