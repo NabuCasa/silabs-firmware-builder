91ed256bb071fa380bc3e7520573645ead1c927a
puddly <32534428+puddly@users.noreply.github.com>
2024-05-29 12:59:13 -0400

    Remove `_OVERRIDE` suffix from board and manuf name

--- src/ncp-uart-hw/config/xncp_config.h
+++ src/ncp-uart-hw/config/xncp_config.h
@@ -10,7 +10,7 @@
 // Some manufacturers do not write a board or manufacturer name to the NCP.
 // Rather than writing the manufacturing tokens within the application, you can instead
 // supply overrides that will be preferred to the manufacturing token values.
-#define XNCP_BOARD_NAME_OVERRIDE           ("")
-#define XNCP_MANUF_NAME_OVERRIDE           ("")
+#define XNCP_BOARD_NAME  ("")
+#define XNCP_MANUF_NAME  ("")
 
 #endif /* CONFIG_XNCP_CONFIG_H_ */
