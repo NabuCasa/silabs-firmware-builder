946740f2e3577519a94d3269c235b073a3b3fbaa
puddly <32534428+puddly@users.noreply.github.com>
2024-05-29 12:46:57 -0400

    Prefix XNCP config

--- src/zigbee_ncp/config/xncp_config.h
+++ src/zigbee_ncp/config/xncp_config.h
@@ -4,6 +4,6 @@
 // Table entries are ephemeral and are expected to be populated before a request is sent.
 // This is not the size of any source route table! Rather, this controls how many unique
 // destinations can be concurrently contacted with source routing enabled.
-#define MANUAL_SOURCE_ROUTE_TABLE_SIZE  (20)
+#define XNCP_MANUAL_SOURCE_ROUTE_TABLE_SIZE  (20)
 
 #endif /* CONFIG_XNCP_CONFIG_H_ */
