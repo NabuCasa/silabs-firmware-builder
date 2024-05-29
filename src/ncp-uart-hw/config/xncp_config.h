de82544d8fc7ff47e11cb3d36ce76c52ea9a37e4
puddly <32534428+puddly@users.noreply.github.com>
2024-05-29 12:48:22 -0400

    Allow overriding the board and manufacturer name in-firmware

--- src/ncp-uart-hw/config/xncp_config.h
+++ src/ncp-uart-hw/config/xncp_config.h
@@ -6,4 +6,12 @@
 // destinations can be concurrently contacted with source routing enabled.
 #define XNCP_MANUAL_SOURCE_ROUTE_TABLE_SIZE  (20)
 
+
+// Some manufacturers do not write a board or manufacturer name to the NCP.
+// Rather than writing the manufacturing tokens within the application, you can instead
+// supply overrides that will be preferred to the manufacturing token values.
+#define XNCP_BOARD_MANUF_OVERRIDE_ENABLED  (false)
+#define XNCP_BOARD_NAME_OVERRIDE           ("")
+#define XNCP_MANUF_NAME_OVERRIDE           ("")
+
 #endif /* CONFIG_XNCP_CONFIG_H_ */
