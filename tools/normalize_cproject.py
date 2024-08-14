1b5deb372b1e81d0cd7c9fdd54f050ebed8b4875
puddly <32534428+puddly@users.noreply.github.com>
2024-08-14 14:45:49 -0400

    One more pass

--- tools/normalize_cproject.py
+++ tools/normalize_cproject.py
@@ -5,6 +5,9 @@ import pathlib
 import xml.etree.ElementTree as ET
 
 
+NEWLINE_SENTINEL = "7200872e315d2518866d8a02e8258034"
+
+
 def json_dumps(obj: dict | list) -> str:
     """Compactly dump JSON into a string."""
     return json.dumps(obj, separators=(", ", ": "), indent=4)
@@ -39,6 +42,11 @@ for storage_module in tree.findall(".//storageModule"):
 
                 state["resolvedOptionsStr"] = json_dumps(resolved_options)
 
+            if "builtinIncludesStr" in state:
+                state["builtinIncludesStr"] = NEWLINE_SENTINEL.join(
+                    state["builtinIncludesStr"].split()
+                )
+
         storage_module.attrib["cppBuildConfig.projectBuiltInState"] = json_dumps(
             project_built_in_state
         )
@@ -55,10 +63,10 @@ for storage_module in tree.findall(".//storageModule"):
 xml_text = ET.tostring(tree, encoding="unicode", xml_declaration=False)
 xml_text = xml_text.replace(" />", "/>")
 
-# Replace newlines with literals!
+# Replace newlines with literals
 xml_text = xml_text.replace("&#10;", "\n")
-# xml_text.replace("\\n", "\n\\n")
-xml_text = re.sub(r"\s+\\n\s+", "\n\\n", xml_text, flags=re.MULTILINE)
+xml_text = xml_text.replace(NEWLINE_SENTINEL, "\n")
+xml_text = re.sub(r"\s*\\n\s*", "\n\\n", xml_text, flags=re.MULTILINE)
 
 # Only touch the filesystem if we need to
 if processing_instructions + xml_text != cproject:
