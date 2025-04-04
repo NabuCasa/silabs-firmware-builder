4fc25740406f6c69efd942a7f433f7df5398f047
puddly <32534428+puddly@users.noreply.github.com>
2025-04-04 13:01:01 -0400

    Include changelogs in the manifest (#104)

--- tools/create_manifest.py
+++ tools/create_manifest.py
@@ -3,6 +3,7 @@
 
 from __future__ import annotations
 
+import re
 import json
 import logging
 import hashlib
@@ -16,6 +17,23 @@ from universal_silabs_flasher.firmware import parse_firmware_image
 _LOGGER = logging.getLogger(__name__)
 
 
+def parse_markdown_changelog(text: str) -> dict[str, tuple[str, str]]:
+    changelogs = {}
+    chunks = re.split(r"^# (.*?)\n", text, flags=re.MULTILINE)[1:]
+
+    for version, raw_text in zip(chunks[::2], chunks[1::2]):
+        first_line, rest = raw_text.split("\n", 1)
+
+        if len(first_line) > 255:
+            raise ValueError(
+                "First line of every changelog must be less than 255 characters"
+            )
+
+        changelogs[version] = (first_line, rest.strip() or None)
+
+    return changelogs
+
+
 def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
@@ -24,6 +42,11 @@ def main():
         "firmware_dir",
         type=pathlib.Path,
         help="Directory containing firmware images",
+    )
+    parser.add_argument(
+        "source_dir",
+        type=pathlib.Path,
+        help="Directory containing the source tree to identify changelogs",
     )
 
     args = parser.parse_args()
@@ -57,8 +80,33 @@ def main():
                 "size": len(data),
                 "metadata": metadata,
                 "release_notes": None,
+                "release_summary": None,
             }
         )
+
+    for fw in manifest["firmwares"]:
+        if fw["metadata"] is None:
+            continue
+
+        fw_type = fw["metadata"]["fw_type"]
+        changelog_md = args.source_dir / fw_type / "CHANGELOG.md"
+
+        if not changelog_md.exists():
+            continue
+
+        changelogs = parse_markdown_changelog(changelog_md.read_text())
+
+        version_keys = {k for k in fw["metadata"] if k.endswith("_version")} - {
+            "sdk_version",
+            "metadata_version",
+        }
+        assert len(version_keys) == 1
+        version_key = list(version_keys)[0]
+
+        version = fw["metadata"][version_key]
+        release_notes, release_summary = changelogs[version]
+        fw["release_notes"] = release_notes
+        fw["release_summary"] = release_summary
 
     print(json.dumps(manifest, indent=2))
 
