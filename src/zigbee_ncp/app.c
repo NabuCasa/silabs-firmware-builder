6d6ad9632f696cca3c6e73cbd45021fea35b977e
puddly <32534428+puddly@users.noreply.github.com>
2025-07-24 17:30:28 -0400

    Move to `CHIP_INFO`

--- src/zigbee_ncp/app.c
+++ src/zigbee_ncp/app.c
@@ -42,7 +42,7 @@
 #define FEATURE_MFG_TOKEN_OVERRIDES   (0b00000000000000000000000000000100)
 #define FEATURE_BUILD_STRING          (0b00000000000000000000000000001000)
 #define FEATURE_FLOW_CONTROL_TYPE     (0b00000000000000000000000000010000)
-#define FEATURE_PART_NUMBER           (0b00000000000000000000000000100000)
+#define FEATURE_CHIP_INFO             (0b00000000000000000000000000100000)
 
 #define FEATURE_LED_CONTROL           (0b10000000000000000000000000000000)
 
@@ -52,11 +52,9 @@
     | FEATURE_MFG_TOKEN_OVERRIDES \
     | FEATURE_BUILD_STRING \
     | FEATURE_FLOW_CONTROL_TYPE \
-    | FEATURE_PART_NUMBER \
+    | FEATURE_CHIP_INFO \
     | FEATURE_LED_CONTROL \
 )
-
-static const char part_number_str[] = PART_NUMBER;
 
 extern sli_zigbee_route_table_entry_t sli_zigbee_route_table[];
 extern uint8_t sli_zigbee_route_table_size;
@@ -84,7 +82,7 @@ typedef enum {
   XNCP_CMD_GET_MFG_TOKEN_OVERRIDE_REQ = 0x0002,
   XNCP_CMD_GET_BUILD_STRING_REQ       = 0x0003,
   XNCP_CMD_GET_FLOW_CONTROL_TYPE_REQ  = 0x0004,
-  XNCP_CMD_GET_PART_NUMBER_REQ        = 0x0005,
+  XNCP_CMD_GET_CHIP_INFO_REQ          = 0x0005,
 
   XNCP_CMD_SET_LED_STATE_REQ          = 0x0F00,
   XNCP_CMD_GET_ACCELEROMETER_REQ      = 0x0F01,
@@ -94,7 +92,7 @@ typedef enum {
   XNCP_CMD_GET_MFG_TOKEN_OVERRIDE_RSP = XNCP_CMD_GET_MFG_TOKEN_OVERRIDE_REQ | 0x8000,
   XNCP_CMD_GET_BUILD_STRING_RSP       = XNCP_CMD_GET_BUILD_STRING_REQ       | 0x8000,
   XNCP_CMD_GET_FLOW_CONTROL_TYPE_RSP  = XNCP_CMD_GET_FLOW_CONTROL_TYPE_REQ  | 0x8000,
-  XNCP_CMD_GET_PART_NUMBER_RSP        = XNCP_CMD_GET_PART_NUMBER_REQ        | 0x8000,
+  XNCP_CMD_GET_CHIP_INFO_RSP          = XNCP_CMD_GET_CHIP_INFO_REQ        | 0x8000,
 
   XNCP_CMD_SET_LED_STATE_RSP          = XNCP_CMD_SET_LED_STATE_REQ          | 0x8000,
   XNCP_CMD_GET_ACCELEROMETER_RSP      = XNCP_CMD_GET_ACCELEROMETER_REQ      | 0x8000,
@@ -433,12 +431,22 @@ EmberStatus emberAfPluginXncpIncomingCustomFrameCallback(uint8_t messageLength,
       break;
     }
 
-    case XNCP_CMD_GET_PART_NUMBER_REQ: {
-      rsp_command_id = XNCP_CMD_GET_PART_NUMBER_RSP;
+    case XNCP_CMD_GET_CHIP_INFO_REQ: {
+      rsp_command_id = XNCP_CMD_GET_CHIP_INFO_RSP;
       rsp_status = EMBER_SUCCESS;
 
-      replyPayloadLength += strlen(part_number_str);
-      memcpy(replyPayload + *replyPayloadLength, part_number_str, strlen(part_number_str));
+      // RAM size
+      replyPayload[(*replyPayloadLength)++] = (uint8_t)((RAM_MEM_SIZE >>  0) & 0xFF);
+      replyPayload[(*replyPayloadLength)++] = (uint8_t)((RAM_MEM_SIZE >>  8) & 0xFF);
+      replyPayload[(*replyPayloadLength)++] = (uint8_t)((RAM_MEM_SIZE >> 16) & 0xFF);
+      replyPayload[(*replyPayloadLength)++] = (uint8_t)((RAM_MEM_SIZE >> 24) & 0xFF);
+
+      // Part number
+      uint8_t value_length = strlen(PART_NUMBER);
+      replyPayload[(*replyPayloadLength)++] = value_length;
+
+      memcpy(replyPayload + *replyPayloadLength, PART_NUMBER, value_length);
+      *replyPayloadLength += value_length;
       break;
     }
 
