b3cc5e293874c93fcfbd4461701da5078ff58273
tansony72 <116590635+tansony72@users.noreply.github.com>
2025-09-13 20:47:26 +0800

    Update xncp_config.h

--- src/zigbee_ncp/config/xncp_config.h
+++ src/zigbee_ncp/config/xncp_config.h
@@ -10,8 +10,8 @@
 // Some manufacturers do not write a board or manufacturer name to the NCP.
 // Rather than writing the manufacturing tokens within the application, you can instead
 // supply overrides that will be preferred to the manufacturing token values.
-#define XNCP_MFG_MANUF_NAME  ("")
-#define XNCP_MFG_BOARD_NAME  ("")
+#define XNCP_MFG_MANUF_NAME  ("DIY")
+#define XNCP_MFG_BOARD_NAME  ("ZBGW7688")
 
 
 // Specify a build string that can be read by the host, augmenting its version info
