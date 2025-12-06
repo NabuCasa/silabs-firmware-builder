00ce150b7fe9c39d6b7307ddc9ce47c76707622b
puddly <32534428+puddly@users.noreply.github.com>
2025-12-05 20:21:07 -0500

    Fix defaults for XNCP config

--- src/zigbee_ncp/extension/xncp_common_extension/config/xncp_config.h
+++ src/zigbee_ncp/extension/xncp_common_extension/config/xncp_config.h
@@ -5,6 +5,8 @@
 #ifndef XNCP_CONFIG_H_
 #define XNCP_CONFIG_H_
 
+#include "sl_iostream_usart_vcom_config.h"
+
 // <<< Use Configuration Wizard in Context Menu >>>
 
 // <h>XNCP Common Commands Configuration
@@ -13,38 +15,32 @@
 // <i> Number of manual source routes that can be stored
 // <i> Default: 200
 #ifndef XNCP_MANUAL_SOURCE_ROUTE_TABLE_SIZE
-#warning "XNCP manual source route table size not configured"
-// #define XNCP_MANUAL_SOURCE_ROUTE_TABLE_SIZE 200
+#define XNCP_MANUAL_SOURCE_ROUTE_TABLE_SIZE 200
 #endif
 
 // <s XNCP_MFG_MANUF_NAME> Manufacturer name override
 // <i> String returned for EZSP_MFG_STRING token override
 #ifndef XNCP_MFG_MANUF_NAME
-#warning "XNCP manufacturer name not configured"
-// #define XNCP_MFG_MANUF_NAME "Unknown"
+#define XNCP_MFG_MANUF_NAME ""
 #endif
 
 // <s XNCP_MFG_BOARD_NAME> Board name override
 // <i> String returned for EZSP_MFG_BOARD_NAME token override
 #ifndef XNCP_MFG_BOARD_NAME
-#warning "XNCP board name not configured"
-// #define XNCP_MFG_BOARD_NAME "Unknown"
+#define XNCP_MFG_BOARD_NAME ""
 #endif
 
 // <s XNCP_BUILD_STRING> Build string
 // <i> Build identifier string (e.g. timestamp)
 #ifndef XNCP_BUILD_STRING
-#warning "XNCP build string not configured"
-// #define XNCP_BUILD_STRING ""
+#define XNCP_BUILD_STRING ""
 #endif
 
 // <o XNCP_FLOW_CONTROL_TYPE> Flow control type
-// <usartHwFlowControlNone=> None (software)
-// <usartHwFlowControlCtsAndRts=> Hardware (CTS/RTS)
-// <i> Default: usartHwFlowControlNone
+// <i> Default: auto-detect from UART config (SL_IOSTREAM_USART_VCOM_FLOW_CONTROL_TYPE)
+// <i> Override only if external flow control differs from internal UART (e.g. ZBT-2)
 #ifndef XNCP_FLOW_CONTROL_TYPE
-#warning "XNCP flow control type not configured"
-// #define XNCP_FLOW_CONTROL_TYPE usartHwFlowControlNone
+#define XNCP_FLOW_CONTROL_TYPE SL_IOSTREAM_USART_VCOM_FLOW_CONTROL_TYPE
 #endif
 
 // <o XNCP_EZSP_VERSION_PATCH_NUM_OVERRIDE> EZSP version patch number override
