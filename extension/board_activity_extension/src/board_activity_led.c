e85c1decbde3750921859060f61179565867eb87
puddly <32534428+puddly@users.noreply.github.com>
2025-12-09 11:03:07 -0500

    Fix Yellow Zigbee activity LED (#158)
    * Fix Yellow Zigbee activity LED
    
    * Make the LED configurable

---
+++ extension/board_activity_extension/src/board_activity_led.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+
+#include "board_activity_led_config.h"
+#include "sl_led.h"
+#include "sl_simple_led_instances.h"
+
+void __wrap_halStackIndicateActivity(bool turnOn)
+{
+    if (turnOn) {
+        sl_led_turn_on(&BOARD_ACTIVITY_LED_INSTANCE);
+    } else {
+        sl_led_turn_off(&BOARD_ACTIVITY_LED_INSTANCE);
+    }
+}
