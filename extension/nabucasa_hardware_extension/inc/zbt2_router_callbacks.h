e0c427df61d48ee1c23448b0afac61222e0fd17d
puddly <32534428+puddly@users.noreply.github.com>
2025-12-22 14:55:24 -0500

    Remove unused template contribution

--- extension/nabucasa_hardware_extension/inc/zbt2_router_callbacks.h
+++ extension/nabucasa_hardware_extension/inc/zbt2_router_callbacks.h
@@ -23,16 +23,4 @@ void zbt2_router_init(uint8_t init_level);
  */
 void zbt2_router_stack_status_cb(sl_status_t status);
 
-/**
- * @brief Handle network steering completion
- * @param status Result status
- * @param totalBeacons Total beacons heard
- * @param joinAttempts Number of join attempts
- * @param finalState Final steering state
- */
-void zbt2_router_network_steering_complete_cb(sl_status_t status,
-                                               uint8_t totalBeacons,
-                                               uint8_t joinAttempts,
-                                               uint8_t finalState);
-
 #endif // ZBT2_ROUTER_CALLBACKS_H
